841e3f0c0927346d25e8dc5d130a3304b1c4e070
Calvin Gerling <calvin@gerling.sh>
2021-12-26 12:33:06 -0300

    create instruction fixtures

--- test/chip8/instruction_test.exs
+++ test/chip8/instruction_test.exs
@@ -1,23 +1,25 @@
 defmodule Chip8.InstructionTest do
   use ExUnit.Case, async: true
 
+  alias Chip8.Fixture.ArgumentsInstruction
+  alias Chip8.Fixture.IdentityInstruction
   alias Chip8.Instruction
 
   describe "new/1" do
     test "should return a instruction struct" do
-      instruction = Instruction.new(Instruction.RUN)
+      instruction = Instruction.new(IdentityInstruction)
 
       assert %Instruction{} = instruction
     end
 
     test "should return a instruction of the given module" do
-      instruction = Instruction.new(Instruction.RUN)
+      instruction = Instruction.new(IdentityInstruction)
 
-      assert Instruction.RUN == instruction.module
+      assert IdentityInstruction == instruction.module
     end
 
     test "should return a instruction with empty arguments" do
-      instruction = Instruction.new(Instruction.RUN)
+      instruction = Instruction.new(IdentityInstruction)
 
       assert %{} == instruction.arguments
     end
@@ -25,19 +27,19 @@ defmodule Chip8.InstructionTest do
 
   describe "new/2" do
     test "should return a instruction struct" do
-      instruction = Instruction.new(Instruction.RUN, %{foo: :bar})
+      instruction = Instruction.new(ArgumentsInstruction, %{foo: :bar})
 
       assert %Instruction{} = instruction
     end
 
     test "should return a instruction of the given module" do
-      instruction = Instruction.new(Instruction.RUN, %{foo: :bar})
+      instruction = Instruction.new(ArgumentsInstruction, %{foo: :bar})
 
-      assert Instruction.RUN == instruction.module
+      assert ArgumentsInstruction == instruction.module
     end
 
     test "should return a instruction with the given arguments" do
-      instruction = Instruction.new(Instruction.RUN, %{foo: :bar})
+      instruction = Instruction.new(ArgumentsInstruction, %{foo: :bar})
 
       assert %{foo: :bar} == instruction.arguments
     end
