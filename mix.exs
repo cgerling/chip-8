9ea5becd9e4b7ef3446758b98a8a1a32b64b04be
Calvin Gerling <calvin@gerling.sh>
2021-12-26 12:33:29 -0300

    compile modules inside test/support when running tests

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Chip8.MixProject do
       app: :chip8,
       version: "0.1.0",
       elixir: "~> 1.13",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: [
@@ -19,6 +20,9 @@ defmodule Chip8.MixProject do
       extra_applications: [:logger]
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     [
