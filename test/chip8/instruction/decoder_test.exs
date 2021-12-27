1c71f4455c1fd647b1395301746fbc490c1d4d1b
Calvin Gerling <calvin@gerling.sh>
2021-12-27 20:49:38 -0300

    add decoder to instruction DRW Vx, Vy, nibble

--- test/chip8/instruction/decoder_test.exs
+++ test/chip8/instruction/decoder_test.exs
@@ -24,5 +24,15 @@ defmodule Chip8.Instruction.DecoderTest do
       assert Chip8.Instruction.SYS == instruction.module
       assert %{address: 0xA4F} == instruction.arguments
     end
+
+    test "should return a instruction struct for the `DRW Vx, Vy, nibble` instruction" do
+      bytes = [0xD7, 0xF2]
+
+      instruction = Decoder.decode(bytes)
+
+      assert %Instruction{} = instruction
+      assert Chip8.Instruction.DRW == instruction.module
+      assert %{x: 0x7, y: 0xF, nibble: 0x2} == instruction.arguments
+    end
   end
 end
