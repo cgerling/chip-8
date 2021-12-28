987574087e857f20b7b053ef47d79fdc4ab36531
Calvin Gerling <calvin@gerling.sh>
2021-12-28 16:01:42 -0300

    create function to get the value of a v register

--- test/chip8/v_registers_test.exs
+++ test/chip8/v_registers_test.exs
@@ -10,4 +10,24 @@ defmodule Chip8.VRegistersTest do
       assert %VRegisters{} = v_registers
     end
   end
+
+  describe "get/2" do
+    test "should return an integer" do
+      v_registers = VRegisters.new()
+
+      register = :rand.uniform(0xF)
+      value = VRegisters.get(v_registers, register)
+
+      assert is_integer(value)
+    end
+
+    test "should return the value stored in the given register" do
+      v_registers = VRegisters.new()
+
+      register = :rand.uniform(0xF)
+      value = VRegisters.get(v_registers, register)
+
+      assert 0 == value
+    end
+  end
 end
