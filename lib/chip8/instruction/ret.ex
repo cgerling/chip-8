69a8e56713edb43f84be1bc2cd6adcdbe262f9f4
Calvin Gerling <calvin@gerling.sh>
2021-12-29 12:25:36 -0300

    implement instruction RET

---
+++ lib/chip8/instruction/ret.ex
@@ -0,0 +1,16 @@
+defmodule Chip8.Instruction.RET do
+  @moduledoc false
+
+  @behaviour Chip8.Instruction
+
+  alias Chip8.Runtime
+  alias Chip8.Stack
+
+  @impl Chip8.Instruction
+  def execute(%Runtime{} = runtime, %{}) do
+    case Stack.pop(runtime.stack) do
+      {nil, _stack} -> runtime
+      {address, stack} -> %{runtime | pc: address, stack: stack}
+    end
+  end
+end
