38fe0523827dd6f9279f39eb8563a1e2bac336f0
Calvin Gerling <calvin@gerling.sh>
2021-12-29 15:05:01 -0300

    implement instruction SE Vx, Vy

--- test/chip8/instruction/se_test.exs
+++ test/chip8/instruction/se_test.exs
@@ -43,5 +43,36 @@ defmodule Chip8.Instruction.SETest do
 
       assert runtime.pc == executed_runtime.pc
     end
+
+    test "should return a runtime with pc set to next instruction when v register x is equals to v register y" do
+      runtime = Runtime.new()
+      value = :rand.uniform(0xFF)
+      x = 0xE
+      y = 0x4
+
+      v_registers = runtime.v |> VRegisters.set(x, value) |> VRegisters.set(y, value)
+      runtime = put_in(runtime.v, v_registers)
+
+      arguments = %{x: x, y: y}
+      executed_runtime = SE.execute(runtime, arguments)
+
+      assert runtime.pc + 2 == executed_runtime.pc
+    end
+
+    test "should return a runtime with pc unchanged when v register x is not equals to v register y" do
+      runtime = Runtime.new()
+      x = 0xE
+      x_value = 0x95
+      y = 0x4
+      y_value = 0x1A
+
+      v_registers = runtime.v |> VRegisters.set(x, x_value) |> VRegisters.set(y, y_value)
+      runtime = put_in(runtime.v, v_registers)
+
+      arguments = %{x: x, y: y}
+      executed_runtime = SE.execute(runtime, arguments)
+
+      assert runtime.pc == executed_runtime.pc
+    end
   end
 end
