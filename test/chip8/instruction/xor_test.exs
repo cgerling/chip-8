16f5923cef2637d1f69fbf9ba4ce7d6be465a497
Calvin Gerling <calvin@gerling.sh>
2021-12-29 18:04:51 -0300

    implement instruction XOR Vx, Vy

---
+++ test/chip8/instruction/xor_test.exs
@@ -0,0 +1,35 @@
+defmodule Chip8.Instruction.XORTest do
+  use ExUnit.Case, async: true
+
+  alias Chip8.Instruction.XOR
+  alias Chip8.Runtime
+  alias Chip8.VRegisters
+
+  describe "execute/2" do
+    test "should return a runtime struct" do
+      runtime = Runtime.new()
+
+      x = :rand.uniform(0xF)
+      y = :rand.uniform(0xF)
+      arguments = %{x: x, y: y}
+      executed_runtime = XOR.execute(runtime, arguments)
+
+      assert %Runtime{} = executed_runtime
+    end
+
+    test "should return a runtime with v register x set to the result of a bitwise xor of v register x and v register y" do
+      runtime = Runtime.new()
+      x = 0x1
+      x_value = 0x7F
+      y = 0x7
+      y_value = 0x44
+      v_registers = runtime.v |> VRegisters.set(x, x_value) |> VRegisters.set(y, y_value)
+      runtime = put_in(runtime.v, v_registers)
+
+      arguments = %{x: x, y: y}
+      executed_runtime = XOR.execute(runtime, arguments)
+
+      assert 0x3B == VRegisters.get(executed_runtime.v, x)
+    end
+  end
+end
