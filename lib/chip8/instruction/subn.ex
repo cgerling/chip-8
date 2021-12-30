c6429b3b03774b4cbb2073dd2e8f06bb53ac53b8
Calvin Gerling <calvin@gerling.sh>
2021-12-30 09:46:21 -0300

    implement instruction SUBN Vx, Vy

---
+++ lib/chip8/instruction/subn.ex
@@ -0,0 +1,20 @@
+defmodule Chip8.Instruction.SUBN do
+  @moduledoc false
+
+  @behaviour Chip8.Instruction
+
+  alias Chip8.Runtime
+  alias Chip8.VRegisters
+
+  @impl Chip8.Instruction
+  def execute(%Runtime{} = runtime, %{x: x, y: y}) do
+    register_x = VRegisters.get(runtime.v, x)
+    register_y = VRegisters.get(runtime.v, y)
+
+    sub_result = abs(register_y - register_x)
+    carry_flag = if register_y > register_x, do: 1, else: 0
+
+    v_registers = runtime.v |> VRegisters.set(0xF, carry_flag) |> VRegisters.set(x, sub_result)
+    %{runtime | v: v_registers}
+  end
+end
