d54e321ceed5a24252362c52a58055e4cb749cb9
Calvin Gerling <calvin@gerling.sh>
2021-12-30 10:08:14 -0300

    implement instruction RND Vx, byte

---
+++ test/chip8/instruction/rnd_test.exs
@@ -0,0 +1,35 @@
+defmodule Chip8.Instruction.RNDTest do
+  use ExUnit.Case, async: true
+
+  alias Chip8.Instruction.RND
+  alias Chip8.Runtime
+  alias Chip8.VRegisters
+
+  describe "execute/2" do
+    test "should return a runtime struct" do
+      runtime = Runtime.new()
+
+      x = :rand.uniform(0xF)
+      byte = :rand.uniform(0xFF)
+      arguments = %{x: x, byte: byte}
+      executed_runtime = RND.execute(runtime, arguments)
+
+      assert %Runtime{} = executed_runtime
+    end
+
+    test "should return a runtime with v register x set to the result of a bitwise and of a random byte and the given byte" do
+      runtime = Runtime.new()
+
+      x = :rand.uniform(0xF)
+      byte = 0x8A
+
+      seed = {1406, 407_414, 139_258}
+      :rand.seed(:exsss, seed)
+
+      arguments = %{x: x, byte: byte}
+      executed_runtime = RND.execute(runtime, arguments)
+
+      assert 0x8 == VRegisters.get(executed_runtime.v, x)
+    end
+  end
+end
