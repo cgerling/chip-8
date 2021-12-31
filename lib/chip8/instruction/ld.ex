73d529755ccb3aff0e19ba77e359f6b7d5fa18ed
Calvin Gerling <calvin@gerling.sh>
2021-12-31 02:22:42 -0300

    remove metadata from LD instruction arguments

--- lib/chip8/instruction/ld.ex
+++ lib/chip8/instruction/ld.ex
@@ -11,22 +11,22 @@ defmodule Chip8.Instruction.LD do
   alias Chip8.VRegisters
 
   @impl Chip8.Instruction
-  def execute(%Runtime{} = runtime, %{x: x, operation: :bcd}) do
-    register_x = VRegisters.get(runtime.v, x)
-    decimal_digits = Integer.digits(register_x, 10)
+  def execute(%Runtime{} = runtime, %{x: :bcd, y: y}) do
+    register_y = VRegisters.get(runtime.v, y)
+    decimal_digits = Integer.digits(register_y, 10)
 
     memory = Memory.write(runtime.memory, runtime.i, decimal_digits)
     %{runtime | memory: memory}
   end
 
-  def execute(%Runtime{} = runtime, %{x: x, operation: :store, to: :memory}) do
-    registers = Enum.map(0..x, &VRegisters.get(runtime.v, &1))
+  def execute(%Runtime{} = runtime, %{x: :memory, y: y}) do
+    registers = Enum.map(0..y, &VRegisters.get(runtime.v, &1))
 
     memory = Memory.write(runtime.memory, runtime.i, registers)
     %{runtime | memory: memory}
   end
 
-  def execute(%Runtime{} = runtime, %{x: x, operation: :load, from: :memory}) do
+  def execute(%Runtime{} = runtime, %{x: x, y: :memory}) do
     data = Memory.read(runtime.memory, runtime.i, x + 1)
 
     v_registers =
@@ -39,9 +39,30 @@ defmodule Chip8.Instruction.LD do
     %{runtime | v: v_registers}
   end
 
-  def execute(%Runtime{} = runtime, %{x: x, byte: byte}) do
-    v_registers = VRegisters.set(runtime.v, x, byte)
+  def execute(%Runtime{} = runtime, %{x: x, y: :dt}) do
+    v_registers = VRegisters.set(runtime.v, x, runtime.dt)
     %{runtime | v: v_registers}
+  end
+
+  def execute(%Runtime{} = runtime, %{x: :dt, y: y}) do
+    register_y = VRegisters.get(runtime.v, y)
+    %{runtime | dt: register_y}
+  end
+
+  def execute(%Runtime{} = runtime, %{x: :st, y: y}) do
+    register_y = VRegisters.get(runtime.v, y)
+    %{runtime | st: register_y}
+  end
+
+  def execute(%Runtime{} = runtime, %{x: x, y: :keyboard}) do
+    key_pressed = Keyboard.keys() |> Enum.find(&Keyboard.is_pressed?(runtime.keyboard, &1))
+
+    if is_nil(key_pressed) do
+      %{runtime | pc: runtime.pc - Instruction.byte_size()}
+    else
+      v_registers = VRegisters.set(runtime.v, x, key_pressed)
+      %{runtime | v: v_registers}
+    end
   end
 
   def execute(%Runtime{} = runtime, %{x: x, y: y}) do
@@ -51,34 +72,13 @@ defmodule Chip8.Instruction.LD do
     %{runtime | v: v_registers}
   end
 
-  def execute(%Runtime{} = runtime, %{address: address}) do
-    %{runtime | i: address}
-  end
-
-  def execute(%Runtime{} = runtime, %{x: x, operation: :store, from: :dt}) do
-    v_registers = VRegisters.set(runtime.v, x, runtime.dt)
+  def execute(%Runtime{} = runtime, %{x: x, byte: byte}) do
+    v_registers = VRegisters.set(runtime.v, x, byte)
     %{runtime | v: v_registers}
   end
 
-  def execute(%Runtime{} = runtime, %{x: x, operation: :store, to: :dt}) do
-    register_x = VRegisters.get(runtime.v, x)
-    %{runtime | dt: register_x}
-  end
-
-  def execute(%Runtime{} = runtime, %{x: x, operation: :store, to: :st}) do
-    register_x = VRegisters.get(runtime.v, x)
-    %{runtime | st: register_x}
-  end
-
-  def execute(%Runtime{} = runtime, %{x: x, operation: :store, from: :keyboard}) do
-    key_pressed = Keyboard.keys() |> Enum.find(&Keyboard.is_pressed?(runtime.keyboard, &1))
-
-    if is_nil(key_pressed) do
-      %{runtime | pc: runtime.pc - Instruction.byte_size()}
-    else
-      v_registers = VRegisters.set(runtime.v, x, key_pressed)
-      %{runtime | v: v_registers}
-    end
+  def execute(%Runtime{} = runtime, %{address: address}) do
+    %{runtime | i: address}
   end
 
   def execute(%Runtime{} = runtime, %{x: x}) do
