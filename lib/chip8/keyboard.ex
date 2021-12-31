584c7608087668ad67aae0bc689b21a1d950caf8
Calvin Gerling <calvin@gerling.sh>
2021-12-31 00:45:29 -0300

    create function to check that a key is pressed

--- lib/chip8/keyboard.ex
+++ lib/chip8/keyboard.ex
@@ -5,7 +5,7 @@ defmodule Chip8.Keyboard do
   defstruct @enforce_keys
 
   @type key :: 0x0..0xF
-  @type key_states :: :none
+  @type key_states :: :none | :pressed
 
   @type t :: %__MODULE__{
           keys: %{key() => key_states()}
@@ -16,5 +16,10 @@ defmodule Chip8.Keyboard do
     keys = Map.new(0x0..0xF, &{&1, :none})
 
     %__MODULE__{keys: keys}
+  end
+
+  @spec is_pressed?(t(), key()) :: boolean()
+  def is_pressed?(%__MODULE__{} = keyboard, key) when is_integer(key) and key in 0x0..0xF do
+    keyboard.keys[key] == :pressed
   end
 end
--- test/chip8/keyboard_test.exs
+++ test/chip8/keyboard_test.exs
@@ -10,4 +10,25 @@ defmodule Chip8.KeyboardTest do
       assert %Keyboard{} = keyboard
     end
   end
+
+  describe "is_pressed?/2" do
+    test "should return true when given key is pressed" do
+      keyboard = Keyboard.new()
+      key = :rand.uniform(0xF)
+      keyboard = put_in(keyboard.keys[key], :pressed)
+
+      is_pressed? = Keyboard.is_pressed?(keyboard, key)
+
+      assert is_pressed?
+    end
+
+    test "should return false when given key is not pressed" do
+      keyboard = Keyboard.new()
+      key = :rand.uniform(0xF)
+
+      is_pressed? = Keyboard.is_pressed?(keyboard, key)
+
+      refute is_pressed?
+    end
+  end
 end
