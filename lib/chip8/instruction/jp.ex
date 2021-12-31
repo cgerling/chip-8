a49f71dc68e37683e9779c7f651857a4907bd68e
Calvin Gerling <calvin@gerling.sh>
2021-12-30 21:20:25 -0300

    implement instruction JP V0, address

--- lib/chip8/instruction/jp.ex
+++ lib/chip8/instruction/jp.ex
@@ -4,8 +4,16 @@ defmodule Chip8.Instruction.JP do
   @behaviour Chip8.Instruction
 
   alias Chip8.Runtime
+  alias Chip8.VRegisters
 
   @impl Chip8.Instruction
+  def execute(%Runtime{} = runtime, %{x: 0, address: address}) do
+    register_0 = VRegisters.get(runtime.v, 0)
+
+    pc = address + register_0
+    %{runtime | pc: pc}
+  end
+
   def execute(%Runtime{} = runtime, %{address: address}) do
     %{runtime | pc: address}
   end
