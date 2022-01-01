db82b39dfff45c09609d2bb04f591b2b3ed113e1
Calvin Gerling <calvin@gerling.sh>
2022-01-01 12:13:56 -0300

    remove test for return type from OR instruction tests

--- test/chip8/instruction/or_test.exs
+++ test/chip8/instruction/or_test.exs
@@ -6,17 +6,6 @@ defmodule Chip8.Instruction.ORTest do
   alias Chip8.VRegisters
 
   describe "execute/2" do
-    test "should return a runtime struct" do
-      runtime = Runtime.new()
-
-      x = :rand.uniform(0xF)
-      y = :rand.uniform(0xF)
-      arguments = %{x: x, y: y}
-      executed_runtime = OR.execute(runtime, arguments)
-
-      assert %Runtime{} = executed_runtime
-    end
-
     test "should return a runtime with v register x set to the result of a bitwise or of v register x and v register y" do
       runtime = Runtime.new()
       x = 0xC
@@ -29,6 +18,7 @@ defmodule Chip8.Instruction.ORTest do
       arguments = %{x: x, y: y}
       executed_runtime = OR.execute(runtime, arguments)
 
+      assert %Runtime{} = executed_runtime
       assert 0xD5 == VRegisters.get(executed_runtime.v, x)
     end
   end
