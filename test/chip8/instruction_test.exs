72a08a0adbe3ceef4428affefc375fc6bbcaacfe
Calvin Gerling <calvin@gerling.sh>
2022-01-01 12:08:00 -0300

    remove test for return type from instruction tests

--- test/chip8/instruction_test.exs
+++ test/chip8/instruction_test.exs
@@ -7,41 +7,21 @@ defmodule Chip8.InstructionTest do
   alias Chip8.Runtime
 
   describe "new/1" do
-    test "should return a instruction struct" do
+    test "should return a instruction struct for the given module" do
       instruction = Instruction.new(IdentityInstruction)
 
       assert %Instruction{} = instruction
-    end
-
-    test "should return a instruction of the given module" do
-      instruction = Instruction.new(IdentityInstruction)
-
       assert IdentityInstruction == instruction.module
-    end
-
-    test "should return a instruction with empty arguments" do
-      instruction = Instruction.new(IdentityInstruction)
-
       assert %{} == instruction.arguments
     end
   end
 
   describe "new/2" do
-    test "should return a instruction struct" do
+    test "should return a instruction struct for the given module with the given arguments" do
       instruction = Instruction.new(ArgumentsInstruction, %{foo: :bar})
 
       assert %Instruction{} = instruction
-    end
-
-    test "should return a instruction of the given module" do
-      instruction = Instruction.new(ArgumentsInstruction, %{foo: :bar})
-
       assert ArgumentsInstruction == instruction.module
-    end
-
-    test "should return a instruction with the given arguments" do
-      instruction = Instruction.new(ArgumentsInstruction, %{foo: :bar})
-
       assert %{foo: :bar} == instruction.arguments
     end
   end
@@ -59,17 +39,7 @@ defmodule Chip8.InstructionTest do
   end
 
   describe "execute/2" do
-    test "should return a runtime struct" do
-      runtime = Runtime.new()
-
-      instruction = Instruction.new(IdentityInstruction)
-
-      executed_runtime = Instruction.execute(instruction, runtime)
-
-      assert %Runtime{} = executed_runtime
-    end
-
-    test "should return a runtime state updated by the given instruction" do
+    test "should return a runtime struct updated by the given instruction" do
       runtime = Runtime.new()
 
       pc_value = :rand.uniform(0xFFFF)
@@ -77,6 +47,7 @@ defmodule Chip8.InstructionTest do
 
       executed_runtime = Instruction.execute(instruction, runtime)
 
+      assert %Runtime{} = executed_runtime
       assert pc_value == executed_runtime.pc
     end
   end
