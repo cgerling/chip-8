bfa489b4e3942dae905cf8163d693ca9e085c504
Calvin Gerling <calvin@gerling.sh>
2022-01-03 21:11:26 -0300

    use runtime function to move to next instruction inside instructions

--- lib/chip8/instruction/sknp.ex
+++ lib/chip8/instruction/sknp.ex
@@ -3,17 +3,13 @@ defmodule Chip8.Instruction.SKNP do
 
   use Chip8.Instruction
 
-  alias Chip8.Instruction
   alias Chip8.Keyboard
   alias Chip8.Runtime
 
   @impl Chip8.Instruction
   def execute(%Runtime{} = runtime, %{x: x}) do
-    skip_value =
-      if Keyboard.is_pressed?(runtime.keyboard, runtime.v[x]),
-        do: 0,
-        else: Instruction.byte_size()
-
-    %{runtime | pc: runtime.pc + skip_value}
+    if Keyboard.is_pressed?(runtime.keyboard, runtime.v[x]),
+      do: runtime,
+      else: Runtime.to_next_instruction(runtime)
   end
 end
--- lib/chip8/instruction/skp.ex
+++ lib/chip8/instruction/skp.ex
@@ -3,17 +3,13 @@ defmodule Chip8.Instruction.SKP do
 
   use Chip8.Instruction
 
-  alias Chip8.Instruction
   alias Chip8.Keyboard
   alias Chip8.Runtime
 
   @impl Chip8.Instruction
   def execute(%Runtime{} = runtime, %{x: x}) do
-    skip_value =
-      if Keyboard.is_pressed?(runtime.keyboard, runtime.v[x]),
-        do: Instruction.byte_size(),
-        else: 0
-
-    %{runtime | pc: runtime.pc + skip_value}
+    if Keyboard.is_pressed?(runtime.keyboard, runtime.v[x]),
+      do: Runtime.to_next_instruction(runtime),
+      else: runtime
   end
 end
