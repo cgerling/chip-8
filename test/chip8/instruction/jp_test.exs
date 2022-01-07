66fe8d3e90cd33c2cac689819b1f27bad9aca8d2
Calvin Gerling <calvin@gerling.sh>
2022-01-06 21:52:04 -0300

    fix jp instruction to wrap integer values

--- test/chip8/instruction/jp_test.exs
+++ test/chip8/instruction/jp_test.exs
@@ -8,7 +8,7 @@ defmodule Chip8.Instruction.JPTest do
   describe "execute/2" do
     test "should return a runtime with pc set to the sum of v register 0 and the given address" do
       runtime = Runtime.new()
-      register_value = :rand.uniform(0xFFF)
+      register_value = :rand.uniform(0xFF)
       v_registers = VRegisters.set(runtime.v, 0x0, register_value)
       runtime = put_in(runtime.v, v_registers)
 
@@ -20,6 +20,20 @@ defmodule Chip8.Instruction.JPTest do
       assert register_value + address == executed_runtime.pc
     end
 
+    test "should return a runtime with pc set to the sum of v register 0 and the given address wrapped to 12 bits" do
+      runtime = Runtime.new()
+      register_value = 0xF1F
+      v_registers = VRegisters.set(runtime.v, 0x0, register_value)
+      runtime = put_in(runtime.v, v_registers)
+
+      address = 0x9E3
+      arguments = %{x: 0, address: address}
+      executed_runtime = JP.execute(runtime, arguments)
+
+      assert %Runtime{} = executed_runtime
+      assert 0x902 == executed_runtime.pc
+    end
+
     test "should return a runtime with pc set to the given address" do
       runtime = Runtime.new()
 
@@ -29,6 +43,18 @@ defmodule Chip8.Instruction.JPTest do
 
       assert %Runtime{} = executed_runtime
       assert address == executed_runtime.pc
+    end
+
+    test "should return a runtime with pc set to the given address wrapped to 12 bits" do
+      runtime = Runtime.new()
+
+      value = :rand.uniform(0xFFF)
+      address = 0xFFF + value
+      arguments = %{address: address}
+      executed_runtime = JP.execute(runtime, arguments)
+
+      assert %Runtime{} = executed_runtime
+      assert value - 1 == executed_runtime.pc
     end
   end
 end
