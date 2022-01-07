be51408fee7108b54273a91cce481954125c5fce
Calvin Gerling <calvin@gerling.sh>
2022-01-06 22:14:24 -0300

    fix subn instruction to wrap integer values

--- lib/chip8/instruction/subn.ex
+++ lib/chip8/instruction/subn.ex
@@ -4,11 +4,12 @@ defmodule Chip8.Instruction.SUBN do
   use Chip8.Instruction
 
   alias Chip8.Runtime
+  alias Chip8.UInt
   alias Chip8.VRegisters
 
   @impl Chip8.Instruction
   def execute(%Runtime{} = runtime, %{x: x, y: y}) do
-    sub_result = abs(runtime.v[y] - runtime.v[x])
+    sub_result = UInt.to_uint8(runtime.v[y] - runtime.v[x])
     carry_flag = if runtime.v[y] > runtime.v[x], do: 1, else: 0
 
     v_registers =
