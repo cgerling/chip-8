11f787e3645906b83a01ca9a399c043b3f52e7a3
Calvin Gerling <calvin@gerling.sh>
2022-01-06 21:46:02 -0300

    fix add instructions to wrap integer values

--- lib/chip8/instruction/add.ex
+++ lib/chip8/instruction/add.ex
@@ -4,24 +4,25 @@ defmodule Chip8.Instruction.ADD do
   use Chip8.Instruction
 
   alias Chip8.Runtime
+  alias Chip8.UInt
   alias Chip8.VRegisters
 
   @impl Chip8.Instruction
   def execute(%Runtime{} = runtime, %{x: :i, y: y}) do
-    add_result = runtime.i + runtime.v[y]
+    add_result = UInt.to_uint16(runtime.i + runtime.v[y])
 
     %{runtime | i: add_result}
   end
 
   def execute(%Runtime{} = runtime, %{x: x, y: y}) do
-    add_result = runtime.v[x] + runtime.v[y]
+    add_result = UInt.to_uint8(runtime.v[x] + runtime.v[y])
 
     v_registers = VRegisters.set(runtime.v, x, add_result)
     %{runtime | v: v_registers}
   end
 
   def execute(%Runtime{} = runtime, %{x: x, byte: byte}) do
-    add_result = runtime.v[x] + byte
+    add_result = UInt.to_uint8(runtime.v[x] + byte)
 
     v_registers = VRegisters.set(runtime.v, x, add_result)
     %{runtime | v: v_registers}
