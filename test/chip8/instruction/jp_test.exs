5b61aeecbf6c500d3ed83d46c972e970ab936af5
Calvin Gerling <calvin@gerling.sh>
2022-01-06 21:48:10 -0300

    reorder jp test cases

--- test/chip8/instruction/jp_test.exs
+++ test/chip8/instruction/jp_test.exs
@@ -6,17 +6,6 @@ defmodule Chip8.Instruction.JPTest do
   alias Chip8.VRegisters
 
   describe "execute/2" do
-    test "should return a runtime with pc set to the given address" do
-      runtime = Runtime.new()
-
-      address = :rand.uniform(0xFFF)
-      arguments = %{address: address}
-      executed_runtime = JP.execute(runtime, arguments)
-
-      assert %Runtime{} = executed_runtime
-      assert address == executed_runtime.pc
-    end
-
     test "should return a runtime with pc set to the sum of v register 0 and the given address" do
       runtime = Runtime.new()
       register_value = :rand.uniform(0xFFF)
@@ -29,6 +18,17 @@ defmodule Chip8.Instruction.JPTest do
 
       assert %Runtime{} = executed_runtime
       assert register_value + address == executed_runtime.pc
+    end
+
+    test "should return a runtime with pc set to the given address" do
+      runtime = Runtime.new()
+
+      address = :rand.uniform(0xFFF)
+      arguments = %{address: address}
+      executed_runtime = JP.execute(runtime, arguments)
+
+      assert %Runtime{} = executed_runtime
+      assert address == executed_runtime.pc
     end
   end
 end
