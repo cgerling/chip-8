17e8e1484931b01128789a35f687573c90a67ed7
Calvin Gerling <calvin@gerling.sh>
2022-01-09 09:25:42 -0300

    create function to retrieve a font's character byte size

--- lib/chip8/font.ex
+++ lib/chip8/font.ex
@@ -7,4 +7,7 @@ defmodule Chip8.Font do
   @spec address(0x0..0xF) :: non_neg_integer()
   def address(character) when is_integer(character) and character in 0x0..0xF,
     do: @base_address + @byte_size * character
+
+  @spec character_byte_size() :: pos_integer()
+  def character_byte_size, do: 5
 end
--- test/chip8/font_test.exs
+++ test/chip8/font_test.exs
@@ -32,4 +32,12 @@ defmodule Chip8.FontTest do
       end
     end
   end
+
+  describe "character_byte_size/0" do
+    test "should return the size in bytes of a character" do
+      character_byte_size = Font.character_byte_size()
+
+      assert character_byte_size == 5
+    end
+  end
 end
