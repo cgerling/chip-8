021955c869ef880dbd51125f75ec14c47e09569e
Calvin Gerling <calvin@gerling.sh>
2022-01-09 09:30:09 -0300

    delete Font.address/1 function

--- lib/chip8/font.ex
+++ lib/chip8/font.ex
@@ -1,13 +1,6 @@
 defmodule Chip8.Font do
   @moduledoc false
 
-  @base_address 0x050
-  @byte_size 5
-
-  @spec address(0x0..0xF) :: non_neg_integer()
-  def address(character) when is_integer(character) and character in 0x0..0xF,
-    do: @base_address + @byte_size * character
-
   @spec character_byte_size() :: pos_integer()
   def character_byte_size, do: 5
 end
--- test/chip8/font_test.exs
+++ test/chip8/font_test.exs
@@ -3,36 +3,6 @@ defmodule Chip8.FontTest do
 
   alias Chip8.Font
 
-  describe "address/1" do
-    test "should return the memory address for all characters" do
-      characters_with_address = [
-        {0x0, 0x050},
-        {0x1, 0x055},
-        {0x2, 0x05A},
-        {0x3, 0x05F},
-        {0x4, 0x064},
-        {0x5, 0x069},
-        {0x6, 0x06E},
-        {0x7, 0x073},
-        {0x8, 0x078},
-        {0x9, 0x07D},
-        {0xA, 0x082},
-        {0xB, 0x087},
-        {0xC, 0x08C},
-        {0xD, 0x091},
-        {0xE, 0x096},
-        {0xF, 0x09B}
-      ]
-
-      for {character, character_address} <- characters_with_address do
-        address = Font.address(character)
-
-        assert is_integer(address)
-        assert character_address == address
-      end
-    end
-  end
-
   describe "character_byte_size/0" do
     test "should return the size in bytes of a character" do
       character_byte_size = Font.character_byte_size()
