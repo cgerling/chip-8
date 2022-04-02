a723feb97ab71334c5b20a61496756b751dfb96b
Calvin Gerling <calvin@gerling.sh>
2022-04-02 11:25:04 -0300

    create Argument.Byte struct

---
+++ lib/chip8/instruction/argument/byte.ex
@@ -0,0 +1,27 @@
+defmodule Chip8.Instruction.Argument.Byte do
+  @moduledoc false
+
+  @enforce_keys [:value]
+  defstruct @enforce_keys
+
+  @type value_range :: 0x00..0xFF
+
+  @type t :: %__MODULE__{
+          value: value_range()
+        }
+
+  @spec new(0x0..0xF, 0x0..0xF) :: t()
+  def new(byte1, byte2)
+      when is_integer(byte1) and is_integer(byte2) and byte1 in 0x0..0xF and byte2 in 0x0..0xF do
+    [byte1, byte2]
+    |> Integer.undigits(16)
+    |> new()
+  end
+
+  @spec new(value_range()) :: t()
+  def new(value) when is_integer(value) and value in 0x00..0xFF do
+    %__MODULE__{
+      value: value
+    }
+  end
+end
