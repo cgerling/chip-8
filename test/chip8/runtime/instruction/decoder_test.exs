e2e19d3672415fcb436f8e9a301ff478b7598aff
Calvin Gerling <calvin@gerling.sh>
2022-04-06 21:18:49 -0300

    change Decoder test cases name to better describe the scenario

--- test/chip8/runtime/instruction/decoder_test.exs
+++ test/chip8/runtime/instruction/decoder_test.exs
@@ -9,7 +9,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
   alias Chip8.Runtime.Instruction.Decoder
 
   describe "decode/1" do
-    test "should return a instruction struct for `CLS` instruction" do
+    test "should return a instruction struct when the given bytes match the `CLS` instruction" do
       bytes = build_instruction_bytes(0x00E0)
 
       instruction = Decoder.decode(bytes)
@@ -20,7 +20,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {} == instruction.arguments
     end
 
-    test "should return a instruction struct for `RET` instruction" do
+    test "should return a instruction struct when the given bytes match the `RET` instruction" do
       bytes = build_instruction_bytes(0x00EE)
 
       instruction = Decoder.decode(bytes)
@@ -31,7 +31,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {} == instruction.arguments
     end
 
-    test "should return a instruction struct for `SYS address` instruction" do
+    test "should return a instruction struct when the given bytes match the `SYS address` instruction" do
       bytes = build_instruction_bytes(0x0A4F)
 
       instruction = Decoder.decode(bytes)
@@ -42,7 +42,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Address.new(0xA4F)} == instruction.arguments
     end
 
-    test "should return a instruction struct for `JP address` instruction" do
+    test "should return a instruction struct when the given bytes match the `JP address` instruction" do
       bytes = build_instruction_bytes(0x14ED)
 
       instruction = Decoder.decode(bytes)
@@ -53,7 +53,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Address.new(0x4ED)} == instruction.arguments
     end
 
-    test "should return a instruction struct for `CALL address` instruction" do
+    test "should return a instruction struct when the given bytes match the `CALL address` instruction" do
       bytes = build_instruction_bytes(0x2B06)
 
       instruction = Decoder.decode(bytes)
@@ -64,7 +64,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Address.new(0xB06)} == instruction.arguments
     end
 
-    test "should return a instruction struct for `SE Vx, byte` instruction" do
+    test "should return a instruction struct when the given bytes match the `SE Vx, byte` instruction" do
       bytes = build_instruction_bytes(0x33A8)
 
       instruction = Decoder.decode(bytes)
@@ -75,7 +75,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0x3), Byte.new(0xA8)} == instruction.arguments
     end
 
-    test "should return a instruction struct for `SNE Vx, byte` instruction" do
+    test "should return a instruction struct when the given bytes match the `SNE Vx, byte` instruction" do
       bytes = build_instruction_bytes(0x498F)
 
       instruction = Decoder.decode(bytes)
@@ -86,7 +86,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0x9), Byte.new(0x8F)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `SE Vx, Vy` instruction" do
+    test "should return a instruction struct when the given bytes match the `SE Vx, Vy` instruction" do
       bytes = build_instruction_bytes(0x5E00)
 
       instruction = Decoder.decode(bytes)
@@ -97,7 +97,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0xE), Register.v(0x0)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `LD Vx, byte` instruction" do
+    test "should return a instruction struct when the given bytes match the `LD Vx, byte` instruction" do
       bytes = build_instruction_bytes(0x6629)
 
       instruction = Decoder.decode(bytes)
@@ -108,7 +108,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0x6), Byte.new(0x29)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `ADD Vx, byte` instruction" do
+    test "should return a instruction struct when the given bytes match the `ADD Vx, byte` instruction" do
       bytes = build_instruction_bytes(0x75DB)
 
       instruction = Decoder.decode(bytes)
@@ -119,7 +119,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0x5), Byte.new(0xDB)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `LD Vx, Vy` instruction" do
+    test "should return a instruction struct when the given bytes match the `LD Vx, Vy` instruction" do
       bytes = build_instruction_bytes(0x8970)
 
       instruction = Decoder.decode(bytes)
@@ -130,7 +130,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0x9), Register.v(0x7)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `OR Vx, Vy` instruction" do
+    test "should return a instruction struct when the given bytes match the `OR Vx, Vy` instruction" do
       bytes = build_instruction_bytes(0x8A31)
 
       instruction = Decoder.decode(bytes)
@@ -141,7 +141,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0xA), Register.v(0x3)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `AND Vx, Vy` instruction" do
+    test "should return a instruction struct when the given bytes match the `AND Vx, Vy` instruction" do
       bytes = build_instruction_bytes(0x86D2)
 
       instruction = Decoder.decode(bytes)
@@ -152,7 +152,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0x6), Register.v(0xD)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `XOR Vx, Vy` instruction" do
+    test "should return a instruction struct when the given bytes match the `XOR Vx, Vy` instruction" do
       bytes = build_instruction_bytes(0x8C03)
 
       instruction = Decoder.decode(bytes)
@@ -163,7 +163,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0xC), Register.v(0x0)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `ADD Vx, Vy` instruction" do
+    test "should return a instruction struct when the given bytes match the `ADD Vx, Vy` instruction" do
       bytes = build_instruction_bytes(0x8F74)
 
       instruction = Decoder.decode(bytes)
@@ -174,7 +174,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0xF), Register.v(0x7)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `SUB Vx, Vy` instruction" do
+    test "should return a instruction struct when the given bytes match the `SUB Vx, Vy` instruction" do
       bytes = build_instruction_bytes(0x85B5)
 
       instruction = Decoder.decode(bytes)
@@ -185,7 +185,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0x5), Register.v(0xB)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `SHR Vx, Vy` instruction" do
+    test "should return a instruction struct when the given bytes match the `SHR Vx, Vy` instruction" do
       bytes = build_instruction_bytes(0x8C96)
 
       instruction = Decoder.decode(bytes)
@@ -196,7 +196,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0xC), Register.v(0x9)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `SUBN Vx, Vy` instruction" do
+    test "should return a instruction struct when the given bytes match the `SUBN Vx, Vy` instruction" do
       bytes = build_instruction_bytes(0x8307)
 
       instruction = Decoder.decode(bytes)
@@ -207,7 +207,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0x3), Register.v(0x0)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `SHL Vx, Vy` instruction" do
+    test "should return a instruction struct when the given bytes match the `SHL Vx, Vy` instruction" do
       bytes = build_instruction_bytes(0x8AEE)
 
       instruction = Decoder.decode(bytes)
@@ -218,7 +218,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0xA), Register.v(0xE)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `SNE Vx, Vy` instruction" do
+    test "should return a instruction struct when the given bytes match the `SNE Vx, Vy` instruction" do
       bytes = build_instruction_bytes(0x90D0)
 
       instruction = Decoder.decode(bytes)
@@ -229,7 +229,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0x0), Register.v(0xD)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `LD I, address` instructino" do
+    test "should return a instruction struct when the given bytes match the `LD I, address` instruction" do
       bytes = build_instruction_bytes(0xA3FF)
 
       instruction = Decoder.decode(bytes)
@@ -239,7 +239,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.i(), Address.new(0x3FF)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `JP V0, address` instruction" do
+    test "should return a instruction struct when the given bytes match the `JP V0, address` instruction" do
       bytes = build_instruction_bytes(0xB47B)
 
       instruction = Decoder.decode(bytes)
@@ -250,7 +250,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0x0), Address.new(0x47B)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `RND Vx, byte` instruction" do
+    test "should return a instruction struct when the given bytes match the `RND Vx, byte` instruction" do
       bytes = build_instruction_bytes(0xC86A)
 
       instruction = Decoder.decode(bytes)
@@ -261,7 +261,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0x8), Byte.new(0x6A)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `DRW Vx, Vy, nibble` instruction" do
+    test "should return a instruction struct when the given bytes match the `DRW Vx, Vy, nibble` instruction" do
       bytes = build_instruction_bytes(0xD7F2)
 
       instruction = Decoder.decode(bytes)
@@ -271,7 +271,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0x7), Register.v(0xF), Nibble.new(0x2)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `SKP Vx` instruction" do
+    test "should return a instruction struct when the given bytes match the `SKP Vx` instruction" do
       bytes = build_instruction_bytes(0xEC9E)
 
       instruction = Decoder.decode(bytes)
@@ -282,7 +282,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0xC)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `SKNP Vx` instruction" do
+    test "should return a instruction struct when the given bytes match the `SKNP Vx` instruction" do
       bytes = build_instruction_bytes(0xEDA1)
 
       instruction = Decoder.decode(bytes)
@@ -293,7 +293,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0xD)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `LD Vx, DT` instruction" do
+    test "should return a instruction struct when the given bytes match the `LD Vx, DT` instruction" do
       bytes = build_instruction_bytes(0xFA07)
 
       instruction = Decoder.decode(bytes)
@@ -304,7 +304,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0xA), Register.dt()} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `LD Vx, K` instruction" do
+    test "should return a instruction struct when the given bytes match the `LD Vx, K` instruction" do
       bytes = build_instruction_bytes(0xF50A)
 
       instruction = Decoder.decode(bytes)
@@ -315,7 +315,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.v(0x5), Register.keyboard()} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `LD DT, Vy` instruction" do
+    test "should return a instruction struct when the given bytes match the `LD DT, Vy` instruction" do
       bytes = build_instruction_bytes(0xF315)
 
       instruction = Decoder.decode(bytes)
@@ -326,7 +326,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.dt(), Register.v(0x3)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `LD ST, Vy` instruction" do
+    test "should return a instruction struct when the given bytes match the `LD ST, Vy` instruction" do
       bytes = build_instruction_bytes(0xFF18)
 
       instruction = Decoder.decode(bytes)
@@ -337,7 +337,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.st(), Register.v(0xF)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `ADD I, Vx` instruction" do
+    test "should return a instruction struct when the given bytes match the `ADD I, Vx` instruction" do
       bytes = build_instruction_bytes(0xF21E)
 
       instruction = Decoder.decode(bytes)
@@ -348,7 +348,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.i(), Register.v(0x2)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `LD F, Vx` instruction" do
+    test "should return a instruction struct when the given bytes match the `LD F, Vx` instruction" do
       bytes = build_instruction_bytes(0xF629)
 
       instruction = Decoder.decode(bytes)
@@ -359,7 +359,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.font(), Register.v(0x6)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `LD B, Vy` instruction" do
+    test "should return a instruction struct when the given bytes match the `LD B, Vy` instruction" do
       bytes = build_instruction_bytes(0xF433)
 
       instruction = Decoder.decode(bytes)
@@ -370,7 +370,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.bcd(), Register.v(0x4)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `LD I, Vy` instruction" do
+    test "should return a instruction struct when the given bytes match the `LD I, Vy` instruction" do
       bytes = build_instruction_bytes(0xFD55)
 
       instruction = Decoder.decode(bytes)
@@ -381,7 +381,7 @@ defmodule Chip8.Runtime.Instruction.DecoderTest do
       assert {Register.memory(), Register.v(0xD)} == instruction.arguments
     end
 
-    test "should return a instruction struct for the `LD Vx, I` instruction" do
+    test "should return a instruction struct when the given bytes match the `LD Vx, I` instruction" do
       bytes = build_instruction_bytes(0xFF65)
 
       instruction = Decoder.decode(bytes)
