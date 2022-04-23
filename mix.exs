71199ff6392e6ffad0e5f6d843f5788880213324
Calvin Gerling <calvin@gerling.sh>
2022-04-23 13:45:47 -0300

    configure ex_doc

--- mix.exs
+++ mix.exs
@@ -9,6 +9,7 @@ defmodule Chip8.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      docs: docs(),
       dialyzer: [
         plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
       ]
@@ -29,6 +30,52 @@ defmodule Chip8.MixProject do
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.27", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"],
+      source_ref: "v0.1.0",
+      groups_for_modules: [
+        Interpreter: [
+          Chip8.Runtime.Display,
+          Chip8.Runtime.Display.Sprite,
+          Chip8.Runtime.Font,
+          Chip8.Runtime.Instruction,
+          Chip8.Runtime.Instruction.Argument.Address,
+          Chip8.Runtime.Instruction.Argument.Byte,
+          Chip8.Runtime.Instruction.Argument.Nibble,
+          Chip8.Runtime.Instruction.Argument.Register,
+          Chip8.Runtime.Keyboard,
+          Chip8.Runtime.Memory,
+          Chip8.Runtime.Timer,
+          Chip8.Runtime.VRegisters
+        ],
+        "Instruction Set": [
+          Chip8.Runtime.Instruction.ADD,
+          Chip8.Runtime.Instruction.AND,
+          Chip8.Runtime.Instruction.CALL,
+          Chip8.Runtime.Instruction.CLS,
+          Chip8.Runtime.Instruction.DRW,
+          Chip8.Runtime.Instruction.JP,
+          Chip8.Runtime.Instruction.LD,
+          Chip8.Runtime.Instruction.OR,
+          Chip8.Runtime.Instruction.RET,
+          Chip8.Runtime.Instruction.RND,
+          Chip8.Runtime.Instruction.SE,
+          Chip8.Runtime.Instruction.SHL,
+          Chip8.Runtime.Instruction.SHR,
+          Chip8.Runtime.Instruction.SKNP,
+          Chip8.Runtime.Instruction.SKP,
+          Chip8.Runtime.Instruction.SNE,
+          Chip8.Runtime.Instruction.SUB,
+          Chip8.Runtime.Instruction.SUBN,
+          Chip8.Runtime.Instruction.SYS,
+          Chip8.Runtime.Instruction.XOR
+        ]
+      ]
     ]
   end
 end
