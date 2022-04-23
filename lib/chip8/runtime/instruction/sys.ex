f6838057d49e861d683685df879cf05d93dbb9d5
Calvin Gerling <calvin@gerling.sh>
2022-04-22 21:43:58 -0300

    add documentation to Instruction.SYS

--- lib/chip8/runtime/instruction/sys.ex
+++ lib/chip8/runtime/instruction/sys.ex
@@ -1,5 +1,17 @@
 defmodule Chip8.Runtime.Instruction.SYS do
-  @moduledoc false
+  @moduledoc """
+  Call a native subroutine.
+
+  It is not possible to call the native subroutines that were available in
+  the computers where Chip-8 was introduced so this instruction is
+  essentially a no-op.
+
+  ## Variants
+
+  Opcode  | Mnemonic              | Description
+  :---:   | :---                  | :---
+  `0nnn`  | `SYS address`         | No-op.
+  """
 
   use Chip8.Runtime.Instruction
 
