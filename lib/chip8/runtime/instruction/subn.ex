54de163af7b00e66afdb18e56fe17e5e844f6671
Calvin Gerling <calvin@gerling.sh>
2022-04-22 22:24:38 -0300

    add documentation to Instruction.SUBN

--- lib/chip8/runtime/instruction/subn.ex
+++ lib/chip8/runtime/instruction/subn.ex
@@ -1,5 +1,20 @@
 defmodule Chip8.Runtime.Instruction.SUBN do
-  @moduledoc false
+  @moduledoc """
+  Subtracts the second operand with the first operand.
+
+  This operation always returns an 8-bit number, that is stored in the first
+  operand. When an _underflow_ happens, the result is always wrapped to fit into
+  the specified size.
+
+  In some variants a _borrow_ bit is also returned, when an _underflow_ happens
+  the borrow bit will be `1`, and `0` otherwise.
+
+  ## Variants
+
+  Opcode  | Mnemonic              | Description
+  :---:   | :---                  | :---
+  `8xy7`  | `SUBN Vx, Vy`         | Set `Vx = Vy - Vx` and `VF = NOT borrow`.
+  """
 
   use Chip8.Runtime.Instruction
 
