b8238169f0cbaefe49fa83323f04970bc7ea9d13
Calvin Gerling <calvin@gerling.sh>
2022-06-07 20:15:26 -0300

    fix SHR Vx {, Vy} instruction
    this instruction was operating on top of Vy's value instead of Vx,
    causing unexpected behavior when running programs

--- test/chip8/interpreter/instruction/shr_test.exs
+++ test/chip8/interpreter/instruction/shr_test.exs
@@ -6,34 +6,34 @@ defmodule Chip8.Interpreter.Instruction.SHRTest do
   alias Chip8.Interpreter.Instruction.SHR
 
   describe "execute/2" do
-    test "should return an interpreter with v register F set to the least significant bit of vy" do
+    test "should return an interpreter with v register F set to the least significant bit of vx" do
       interpreter = Interpreter.new()
-      y = 0x2
-      y_value = 0x85
-      interpreter = put_in(interpreter.v[y], y_value)
+      x = 0x2
+      x_value = 0x85
+      interpreter = put_in(interpreter.v[x], x_value)
 
-      vx = %Register{value: 0xA}
-      vy = %Register{value: y}
+      vx = %Register{value: x}
+      vy = %Register{value: 0xA}
       arguments = {vx, vy}
       executed_interpreter = SHR.execute(interpreter, arguments)
 
       assert %Interpreter{} = executed_interpreter
-      assert 1 == executed_interpreter.v[0xF]
+      assert executed_interpreter.v[0xF] == 1
     end
 
-    test "should return an interpreter with vx set to vy shifted one bit to the right" do
+    test "should return an interpreter with vx set to vx shifted one bit to the right" do
       interpreter = Interpreter.new()
-      y = 0x3
-      y_value = 0xAD
-      interpreter = put_in(interpreter.v[y], y_value)
+      x = 0x3
+      x_value = 0xAD
+      interpreter = put_in(interpreter.v[x], x_value)
 
-      vx = %Register{value: 0xC}
-      vy = %Register{value: y}
+      vx = %Register{value: x}
+      vy = %Register{value: 0xC}
       arguments = {vx, vy}
       executed_interpreter = SHR.execute(interpreter, arguments)
 
       assert %Interpreter{} = executed_interpreter
-      assert 0x56 == executed_interpreter.v[vx.value]
+      assert executed_interpreter.v[vx.value] == 0x56
     end
   end
 end
